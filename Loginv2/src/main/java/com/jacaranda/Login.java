e8140d47251297a3d10802adb2e4be86f655ee98
RomanMillan <romandos962m@gmail.com>
2022-09-28 09:58:40 +0200

    arreglo estructura

---
+++ Loginv2/src/main/java/com/jacaranda/Login.java
@@ -0,0 +1,86 @@
+package com.jacaranda;
+
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.DriverManager;
+import java.util.Objects;
+
+public class Login {
+	private String nombre;
+	private String password;
+	
+	public Login(String nombre, String password) {
+		super();
+		this.nombre = nombre;
+		this.password = password;
+		
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	
+	
+
+	
+	public boolean passwordConfirmation() {
+		boolean result=false;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/cartas?useSSL=false","dummy","dummy");
+			
+			//para ver si tiene conexion con la BD
+			DatabaseMetaData infoBD= conexion.getMetaData();
+			System.out.println("Base de datos: " + infoBD.getDatabaseProductName());
+			System.out.println("Version: " + infoBD.getDatabaseProductVersion());
+			
+			//Consultamos la contaseña y el usuario 
+			String query ="SELECT password, nombre FROM USUARIO where password ='"+this.password+"' and nombre ='"+this.nombre+"'";
+			if(query != null) {
+				result = true;
+			}
+			conexion.close();
+			
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+			
+		
+		
+		return result;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(password);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Login other = (Login) obj;
+		return Objects.equals(password, other.password);
+	}
+
+	@Override
+	public String toString() {
+		return "Login [nombre=" + nombre + ", password=" + password + "]";
+	}
+}
---
+++ Loginv2/src/main/webapp/Login.jsp
@@ -0,0 +1,40 @@
+<%@page import="com.jacaranda.Login"%>
+<%@page import="java.sql.DatabaseMetaData"%>
+<%@page import="java.sql.ResultSet"%>
+<%@page import="java.beans.Statement"%>
+<%@page import="java.sql.DriverManager"%>
+<%@page import="java.sql.Connection"%>
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+	<html lang="es">
+	<head>
+		<meta charset="UTF-8">
+		<title>login</title>
+	</head>
+	<body>
+	<% 
+			
+			//cogemos los datos que ha insertado el usuario
+			String user = request.getParameter("user");
+			String password = request.getParameter("password");
+			
+			
+			//comprobamos que esos datos son correctos
+			Login l = new Login(user,password);	
+			if(l.passwordConfirmation()==true){
+				
+				/* HttpSession sesion = request.getSession();
+				sesion.setAttribute("login","true");
+				sesion.setAttribute("user",user); */
+		%>
+				<jsp:forward page="inicio.jsp"></jsp:forward>
+		<% 
+			}else{
+		%>
+				<jsp:forward page="error.jsp?msg='ERROR'"></jsp:forward>
+		<%
+			}
+		%>
+	</body>
+</html>
