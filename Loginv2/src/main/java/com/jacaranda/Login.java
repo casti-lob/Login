370760e433406fc22a9651fddb670fbeaa578539
roman millan <romandos962m@gmail.com>
2022-09-29 20:41:33 +0200

    intentos de validar usuario

--- Loginv2/src/main/java/com/jacaranda/Login.java
+++ Loginv2/src/main/java/com/jacaranda/Login.java
@@ -1,12 +1,13 @@
 package com.jacaranda;
 
 import java.sql.Connection;
-import java.sql.DatabaseMetaData;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.Objects;
 
 import com.mysql.cj.xdevapi.Statement;
+
 
 public class Login {
 	private String name;
@@ -41,17 +42,24 @@ public class Login {
 	public boolean passwordConfirmation() {
 		boolean result=false;
 		try {
-			Class.forName("com.mysql.jdbc.Driver");
-			Connection conexion = DriverManager.getConnection("jdbc:mysql://localhost:3306/cartas?allowPublicKeyRetrieval=true&useSSL=false","root","dummy");
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection cn = DriverManager.getConnection("jdbc:mysql://localhost:3306/cartas?allowPublicKeyRetrieval=true&useSSL=false","root","dummy");
 			
+			PreparedStatement sentencia = cn.prepareStatement("select password, nombre from usuario where password =? and nombre =?");
+			sentencia.setString(1, password);
+			sentencia.setString(2, name);
+			ResultSet rs = sentencia.executeQuery();
+			
+			System.out.println(rs.getString(1)+ " "+ rs.getString(2));
+			rs.close();
+
+			
+			
+			/*
 			//para ver si tiene conexion con la BD
 			DatabaseMetaData infoBD= conexion.getMetaData();
 			System.out.println("Base de datos: " + infoBD.getDatabaseProductName());
 			System.out.println("Version: " + infoBD.getDatabaseProductVersion());
-			
-			Statement instruccion = (Statement) conexion.createStatement();
-			String sentencia = "select password, nombre from usuario where password ='"+this.password+"' and nombre ='"+this.name+"'";
-			ResultSet resultado = ((java.sql.Statement) instruccion).executeQuery(sentencia);
 			
 			//Consultamos la contaseña y el usuario 
 			//String query ="SELECT password, nombre FROM USUARIO where password ='"+this.password+"' and nombre ='"+this.name+"'";
@@ -61,7 +69,7 @@ public class Login {
 				result = true;
 			}
 			conexion.close();
-			
+			*/
 		}catch(Exception e){
 				System.out.println(e.getMessage());
 			}
